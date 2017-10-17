e369e41d3a4255c7c1a8e6f6d2fb64da348d800a
Gerson Yarce Franco <gersonyarcefranco@iMac-Natalia.local>
2017-10-16 22:56:41 -0500

    SCSS support and assets support

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import "../style/app.scss";
+import keenImage from "../assets/imgs/sterlingarcher.jpg";
 
 export default class Hello extends Component {
   render() {
-    return <div>Hello from react</div>;
+    return (
+      <div>
+        Hello from react
+        <img src={keenImage} alt="Commander Keen" />
+      </div>
+    );
   }
 }
 
